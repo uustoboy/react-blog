f7299e55671cae9e89e86bc1e673b159a8a062cf
uustoboy <tianqi_email@163.com>
2019-06-25 23:09:17 +0800

    add

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const Koa = require('koa')
 const next = require('next')
 const Router = require('koa-router');
+const static = require("koa-static");
+const path = require('path')
+
+
+// 静态资源目录对于相对入口文件index.js的路径
+const staticPath = '../static/';
+
+
 
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -10,10 +18,11 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = new Koa()
   const router = new Router();
+  server.use(static(path.join(__dirname, staticPath)));
   router.get("/aa", async ctx => {
     console.log('aa.js');
     await app.render(ctx.req, ctx.res, "/login", ctx.query);
-    
+
     ctx.respond = false;
   });
   router.get("/login", async ctx => {
@@ -22,11 +31,28 @@ app.prepare().then(() => {
     ctx.respond = false;
   });
 
-  // server.use(async (ctx, next) => {
-  //   await handle(ctx.req, ctx.res)
-  //   console.log('2222');
-  //   ctx.respond = false
-  // })
+  router.get("/", async ctx => {
+    await app.render(ctx.req, ctx.res, "/index", ctx.query);
+    console.log("index.js");
+    ctx.respond = false;
+  });
+
+  router.get("/aboutme", async ctx => {
+    await app.render(ctx.req, ctx.res, "/aboutme", ctx.query);
+    console.log("aboutme.js");
+    ctx.respond = false;
+  });
+  router.get("/detail", async ctx => {
+    await app.render(ctx.req, ctx.res, "/detail", ctx.query);
+    console.log("detail.js");
+    ctx.respond = false;
+  });
+
+  server.use(async (ctx, next) => {
+    await handle(ctx.req, ctx.res)
+    console.log('2222');
+    ctx.respond = false
+  })
   server.use(router.routes())
   server.listen(3000, () => {
     console.log('server is running at http://localhost:3000')
